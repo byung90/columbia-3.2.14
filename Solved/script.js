73ac72d54c599f0ad8342d211a16d59dee23ac67
Byungwook Yoon <byung90@gmail.com>
2021-02-11 20:09:02 -0500

    completed

---
+++ Solved/script.js
@@ -0,0 +1,14 @@
+//WRITE YOUR CODE BELOW
+var students = ["Ethan", "Jonathan", "Joe", "Nancy", "Jeff"];
+
+console.log(students.length);
+
+for (var x = 0; x < students.length; x++) {
+  console.log("Great to see you, " + students[x]);
+}
+
+var x = 0;
+while (x < students.length) {
+  console.log("Great to see you, " + students[x]);
+  x++;
+}
